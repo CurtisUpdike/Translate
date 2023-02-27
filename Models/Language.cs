3dd70094eeb3f3d989679d04d2b360ce2dca399a
Curtis Updike <curtis@updike.dev>
2023-02-26 19:16:58 -0500

    Add models

---
+++ Models/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace Translate.Models;
+
+public class Language
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public bool SupportedAsSource { get; set; }
+    public bool SupportedAsTarget { get; set; }
+}
