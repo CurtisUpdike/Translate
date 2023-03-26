cd707192ace4b5a1bd12cf3ba0fafebf6407d3f7
Curtis Updike <cjupdike@icloud.com>
2023-03-25 21:18:07 -0400

    Resolve nullable warnings

--- Models/Language.cs
+++ Models/Language.cs
@@ -2,8 +2,8 @@
 
 public class Language
 {
-    public string Id { get; set; }
-    public string Name { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
     public bool SupportedAsSource { get; set; }
     public bool SupportedAsTarget { get; set; }
 }
