7682485d5842dde8ec17e34e497248147f40b9eb
Curtis Updike <cjupdike@icloud.com>
2024-02-18 16:06:48 -0500

    Refactor

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -5,88 +5,62 @@ let {
   useEffect,
   useRef
 } = React;
-let e = createElement;
 let { createRoot } = ReactDOM;
 
 
-document.addEventListener('DOMContentLoaded', main);
-
-function main() {
+document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('root'))
     .render(withStrictMode(App));
-}
-
-
-// type Language = { language: string; name: string; }
+});
 
 
 function App() {
   let [allSources] = useState([
-    { language: 'detect', name: 'Detect Language'},
-    { language: 'en', name: 'English'},
-    { language: 'es', name: 'Spanish' }
+    { id: 'detect', name: 'Detect Language'},
+    { id: 'en', name: 'English'},
+    { id: 'es', name: 'Spanish' }
   ]);
 
   let [allTargets] = useState([
-    { language: 'en', name: 'English'},
-    { language: 'es', name: 'Spanish' }
+    { id: 'en', name: 'English'},
+    { id: 'es', name: 'Spanish' }
   ]);
 
-  let [source, setSource] = useState({ language: 'detect', name: 'Detect Language'});
+  let [source, setSource] = useState({ id: 'detect', name: 'Detect Language'});
   let [target, setTarget] = useState(null);
   let [translation, setTranslation] = useState('');
-
-  return (
-    e('div', { className: 'text-translation' },
-      e(TranslationInput, { allSources, source, setSource, target, setTranslation }),
-      e(TranslationOutput, { allTargets, target, setTarget, translation })
-    )
-  );
-}
-
-
-function TranslationInput({ allSources, source, setSource, target, setTranslation }) {
   let [characterCount, setCharacterCount] = useState(0);
 
-  function handleInput(e) {
-    setCharacterCount(e.target.value.length);
-    translate(e.target.value);
+  function handleInput(event) {
+    setCharacterCount(event.target.value.length);
+    translate(event.target.value);
   }
 
   async function translate(text) {
-    setTranslation(await fetchTranslation(text, source?.language, target?.language));
+    setTranslation(await fetchTranslation(text, source?.id, target?.id));
   }
-  
-  return (
-    e('div', { className: 'form' },
-      createCharacterLimit(characterCount),
-      e('div', { className: 'detected-language-container'}, 'English'),
-      e(Dropdown, { 
-        languages: allSources, 
-        selected: source,
-        select: setSource
-      }),
-      e(TextArea, { placeholder: 'Enter text', onChange: handleInput })
-    )
-  );
-}
 
-
-function TranslationOutput({ target, setTarget, allTargets, translation }) {
   return (
-    e('div', { className: 'form translation-output' },
-      e(Dropdown, { 
-        languages: allTargets,
-        selected: target,
-        select: setTarget
-      }),
-      e(
-        'div',
-        { className: 'copy-container'}, 
-        e(CopyIcon),
-        e('div', { className: 'tooltiptext' }, 'Copied!')
+    appContainer(
+      inputContainer(
+        dropdown({ 
+          languages: allSources, 
+          selected: source,
+          select: setSource
+        }),
+        translationInput({ onChange: handleInput }),
+        detectedLanguage('English'),
+        characterLimit(characterCount)
       ),
-      e(TextArea, { placeholder: 'Translation', value: translation, readOnly: true })
+      outputContainer(
+        dropdown({ 
+          languages: allTargets,
+          selected: target,
+          select: setTarget
+        }),
+        translationOutput({ value: translation }),
+        copy()
+      )
     )
   );
 }
@@ -107,87 +81,47 @@ function Dropdown(props) {
       if (selectRef.current.contains(event.target))
         setActive(state => !state);
       else if (searcRef.current.contains(event.target))
-       return;
+        return;
       else
         setActive(false);
     }
   });
 
-  let createOption = ({ language, name }) => 
-    createElement(
-      'div',
-      {
-        className: 'option',
-        onClick: () => select({ language, name })
-      },
+  let createOption = ({ id, name }) => 
+    option({
+      onClick: () => select({ id, name }),
       name,
-      createElement(CheckIcon, { isSelected: (language === selected?.language) })
-    );
-
-  let handleSearch = (e) => setSearch(e.target.value);
-  let searchInput = e('div', { className: 'search-bar', ref: searcRef },
-    createTextInputWrapper(
-      createElement(
-        'input',
-        {
-          className: 'text-input',
-          placeholder: 'Search...',
-          type: 'text',
-          onChange: handleSearch
-        }
-      )
-    ),
-    e(ClearSearchIcon)
-  );
+      active: id === selected?.id
+    });
 
   let searchFilter = (l) => l.name.toUpperCase().includes(search.toUpperCase());
+
   let languageOptions = languages.filter(searchFilter).map(createOption);
 
   return (
-    e('div', { className: 'dropdown language-dropdown', 'aria-label': 'Dropdown' }, 
-      e('div', { className: 'selected-item option', ref: selectRef }, 
-        selected?.name || 'Select', 
-        e(OpenCloseArrowIcon, { active })
-      ),
-      e('div', { className: `dropdown-content ${active ? 'active' : ''}` },
-        searchInput,
-        e('div', { className: 'columns' },
-          e('div', { className: 'column', style: { width: '25%' } },
+    drowpdownWrapper(
+      selectedItem({
+        ref: selectRef,
+        name: selected?.name,
+        active
+      }),
+      dropdownContent({ active },
+        searchBar({ 
+          ref: searcRef, 
+          onChange: (e) => setSearch(e.target.value) 
+        }),
+        div({ className: 'columns' },
+          div({ className: 'column', style: { width: '25%' } },
             ...languageOptions
           ),
-          e('div', { className: 'column', style: { width: '25%' } }),
-          e('div', { className: 'column', style: { width: '25%' } }),
-          e('div', { className: 'column', style: { width: '25%' } })
+          div({ className: 'column', style: { width: '25%' } }),
+          div({ className: 'column', style: { width: '25%' } }),
+          div({ className: 'column', style: { width: '25%' } })
         )
       )
     )
   );
 }
-
-
-let TextArea = (props) =>
-  createTextAreaWrapper(
-    createElement('textarea', { className: 'text-area', rows: '10', ...props }));
-
-
-let withStrictMode = (element) =>
-  createElement(StrictMode, null, createElement(element));
-
-
-let createCharacterLimit = (count) =>
-  createElement('div', { className: 'character-limit' }, `${count}/10000`);
-
-let createDrowpdownWrapper = (...children) => 
-    createElement('div', { className: 'dropdown language-dropdown', 'aria-label': 'Dropdown' }, ...children)
-
-let createTextInputWrapper = (...children) =>
-  createElement('div', { className: 'form-item' },
-    createElement('div', { className: 'text-input-wrapper' }, ...children));
-
-
-let createTextAreaWrapper = (...children) =>
-  createElement('div', { className: 'form-item'}, 
-    createElement('div', { className: 'text-area-wrapper' }, ...children));
 
 
 async function fetchTranslation(text, source, target) {
@@ -209,9 +143,89 @@ async function fetchTranslation(text, source, target) {
 }
 
 
-/*
-** Icons
-*/
+let withStrictMode = (element) =>
+  createElement(StrictMode, null, createElement(element));
+
+let div = (props, ...children) =>
+  createElement('div', props, ...children);
+
+let input = (props, ...children) =>
+  createElement('input', props, ...children);
+
+let appContainer = (...children) =>
+  div({ className: 'text-translation' }, ...children);
+
+let inputContainer = (...children) =>
+  div({ className: 'form' }, ...children);
+
+let outputContainer = (...children) =>
+  div({ className: 'form translation-output' }, ...children);
+
+let characterLimit = (count) =>
+  div({ className: 'character-limit' }, `${count}/10000`);
+
+let drowpdownWrapper = (...children) =>
+  div({ className: 'dropdown language-dropdown', 'aria-label': 'Dropdown' }, ...children)
+
+let inputWrapper = (...children) =>
+  div({ className: 'form-item' },
+    div({ className: 'text-input-wrapper' }, ...children));
+
+let textAreaWrapper = (...children) =>
+  div({ className: 'form-item'}, 
+    div({ className: 'text-area-wrapper' }, ...children));
+
+let translationInput = (props) =>
+  textarea({ placeholder: 'Enter text', ...props });
+
+let translationOutput = (props) =>
+  textarea({ placeholder: 'Translation', readOnly: true, ...props });
+
+let textarea = (props) =>
+  textAreaWrapper(
+    createElement('textarea', { className: 'text-area', rows: '10', ...props }));
+
+let dropdown = ({ languages, selected, select }) => 
+  createElement(Dropdown, { languages, selected, select });
+
+let dropdownContent = ({ active }, ...children) =>
+  div({ className: `dropdown-content ${active ? 'active' : ''}` }, ...children);
+
+let selectedItem = ({ ref, name, active}) =>
+  div({ className: 'selected-item option', ref }, 
+    name || 'Select', 
+    arrowIcon({ active })
+  );
+
+let searchInput = (props) =>
+  inputWrapper(input({
+    className: 'text-input',
+    placeholder: 'Search...',
+    type: 'text',
+    ...props
+  }));
+
+let searchBar = ({ ref, onChange }) =>
+  div({ className: 'search-bar', ref },
+    searchInput({ onChange }),
+    clearIcon()
+  );
+
+let option = ({ onClick, active, name }) =>
+  div({ className: 'option', onClick },
+    name,
+    checkIcon({ active })
+  );
+
+let copy = () => 
+  div({ className: 'copy-container'}, 
+    copyIcon(),
+    div({ className: 'tooltiptext' }, 'Copied!')
+  );
+
+let detectedLanguage = (text) =>
+  div({ className: 'detected-language-container'}, text);
+
 
 let svgAttributes = {
   focusable: 'false', 
@@ -225,38 +239,32 @@ let svgAttributes = {
 };
 
 
-let OpenCloseArrowIcon = ({ isOpen }) => (
-  e('div', { className: `icon open-close-arrow ${isOpen ? 'active' : ''}` },
-    e('svg', { ...svgAttributes, viewBox: '0 0 16 16' }, 
-      e('path', { d: 'M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z'})
+let arrowIcon = ({ active }) => 
+  createElement('div', { className: `icon open-close-arrow ${active ? 'active' : ''}` },
+    createElement('svg', { ...svgAttributes, viewBox: '0 0 16 16' }, 
+      createElement('path', { d: 'M8 11L3 6 3.7 5.3 8 9.6 12.3 5.3 13 6z'})
     )
-  )
-);
+  );
 
 
-let ClearSearchIcon = () => (
-  e('svg', { ...svgAttributes, className: 'icon clear-icon', viewBox: '0 0 16 16'},
-    e('path', { d: 'M8,1C4.1,1,1,4.1,1,8s3.1,7,7,7s7-3.1,7-7S11.9,1,8,1z M10.7,11.5L8,8.8l-2.7,2.7l-0.8-0.8L7.2,8L4.5,5.3l0.8-0.8L8,7.2	l2.7-2.7l0.8,0.8L8.8,8l2.7,2.7L10.7,11.5z' })
-  )
-);
+let clearIcon = () =>
+  createElement('svg', { ...svgAttributes, className: 'icon clear-icon', viewBox: '0 0 16 16'},
+    createElement('path', { d: 'M8,1C4.1,1,1,4.1,1,8s3.1,7,7,7s7-3.1,7-7S11.9,1,8,1z M10.7,11.5L8,8.8l-2.7,2.7l-0.8-0.8L7.2,8L4.5,5.3l0.8-0.8L8,7.2	l2.7-2.7l0.8,0.8L8.8,8l2.7,2.7L10.7,11.5z' })
+  );
 
 
-let CheckIcon = ({ isSelected }) => (
-  e(
-    'svg', 
-    { 
+let checkIcon = ({ active }) => 
+  createElement('svg', { 
       ...svgAttributes, 
-      className: `icon check-icon ${isSelected ? 'active' : ''}`, 
+      className: `icon check-icon ${active ? 'active' : ''}`, 
       viewBox: '0 0 32 32' 
     },
-    e('path', { d: 'M12 21.2L4.9 14.1 3.5 15.5 10.6 22.6 12 24 26.1 9.9 24.7 8.4z' })
-  )
-);
+    createElement('path', { d: 'M12 21.2L4.9 14.1 3.5 15.5 10.6 22.6 12 24 26.1 9.9 24.7 8.4z' })
+  );
 
 
-let CopyIcon = () => (
-  e('svg', { ...svgAttributes, viewBox: '0 0 32 32' },
-    e('path', { d: 'M28,10V28H10V10H28m0-2H10a2,2,0,0,0-2,2V28a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V10a2,2,0,0,0-2-2Z' }),
-    e('path', { d: 'M4,18H2V4A2,2,0,0,1,4,2H18V4H4Z' })
-  )
-);
+let copyIcon = () =>
+  createElement('svg', { ...svgAttributes, viewBox: '0 0 32 32' },
+    createElement('path', { d: 'M28,10V28H10V10H28m0-2H10a2,2,0,0,0-2,2V28a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V10a2,2,0,0,0-2-2Z' }),
+    createElement('path', { d: 'M4,18H2V4A2,2,0,0,1,4,2H18V4H4Z' })
+  );
